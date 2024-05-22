45974f6c1e21181fbef0bb37470846cfc581fbae
KalliaVasilikopoulou <98029855+KalliaVasilikopoulou@users.noreply.github.com>
2024-05-22 18:46:35 +0300

    add more file examples

---
+++ user_scripts/file_examples/0 vs 1/similarity/training_configurations.py
@@ -0,0 +1,23 @@
+training_configurations = {
+	"mnist_dataset_portion": 1.0,
+
+	"epochs": 15,
+	"batch_size": 12,
+	"validation_split": 0.1,
+	"dropout_rate": 0.5,
+	"learning_rate": 1e-3,
+	"decay": 1e-5,
+	
+	"neurasp_conf": {
+		"list_of_possible_output_classes": [0,1],
+		"output_type" : "similarity",
+		"classifiers_conf": {
+			"1": {
+				"objects": [0,1],
+				"list_of_object_classes": [0,1],
+				"object_type": "image",
+				"classes_type" : "digit"
+			}
+		}
+	}
+}
---
+++ user_scripts/file_examples/shirts vs trousers/two_shirts/create_classifiers.py
@@ -0,0 +1,17 @@
+import keras
+from keras import layers, models
+
+classifier_input = keras.Input(shape=(28,28,1))
+x = layers.Conv2D(32, (3,3), activation='relu')(classifier_input)
+x = layers.MaxPooling2D((2,2))(x)
+x = layers.Conv2D(64, (3,3), activation='relu')(x)
+x = layers.MaxPooling2D((2,2))(x)
+x = layers.Conv2D(64, (3,3), activation='relu')(x)
+x = layers.Flatten()(x)
+x = layers.Dense(64, activation='relu')(x)
+
+classifier_output = layers.Dense(2, activation = 'softmax')(x)
+
+classifier = models.Model(classifier_input, classifier_output, name='clothing_classifier')
+
+#classifiers = {'1': classifier}
